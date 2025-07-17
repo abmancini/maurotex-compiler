1ad15b277fb120b44b00298da4788c0191c0b52a
Alberto Mancini <alberto@k-teq.com>
2025-07-17 10:09:26 +0200

    adds debina directory

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/maurotex/issues
+# Bug-Submit: https://github.com/<user>/maurotex/issues/new
+# Changelog: https://github.com/<user>/maurotex/blob/master/CHANGES
+# Documentation: https://github.com/<user>/maurotex/wiki
+# Repository-Browse: https://github.com/<user>/maurotex
+# Repository: https://github.com/<user>/maurotex.git
